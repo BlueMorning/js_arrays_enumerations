c7c2737c7d0c53b1a035f94d068e78a3b1226bca
Eric Carles <carles.eric@wanadoo.fr>
2018-02-14 16:48:59 +0000

    concat function test passed

--- array_tasks.js
+++ array_tasks.js
@@ -1,8 +1,19 @@
 const arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+    if((arr1 === null || arr1 === undefined) && (arr2 === null || arr2 === undefined)){
+      return [];
+    }
+    else if((arr1 === null || arr1 === undefined)){
+      return arr2;
+    }
+    else if((arr2 === null || arr2 === undefined)){
+      return arr1;
+    }
+    else{
+      return arr1.concat(arr2);
+    }
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
 
